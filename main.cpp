e9c76e3995d24677406ac48179ffa6577f68a648
lixinrui706 <xli21@mail.csuchico.edu>
2018-02-26 18:17:27 -0800

    q means

--- main.cpp
+++ main.cpp
@@ -11,27 +11,33 @@ int main()
   string item;
   do
   {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-  if(input =='A'||input =='a')
-  {
-  cout<<"What is the item?"<<endl;
-  cin>>item;
-  if(numItems != 5)
-  {
-  list[numItems] = item;
-  numItems++;
-  }
-  else
-  {
-  cout<<"You'll need a bigger list!"<<endl;
-  }
-  }
-  }while(!(input == 'Q'||input == 'q'));
-
+    cout<<"\n==GROCERY LIST MANAGER==";
+    cout<<"\nEnter your choice: ";
+    cout<<"\n (A)dd an item";
+    cout<<"\n (Q)uit";
+    cout<<"\nYour choice (A/Q): ";
+    cin>>input;
+   if(input =='A'||input =='a')
+   {
+    cout<<"What is the item?"<<endl;
+   
+    if(numItems < 5)
+    {
+     cin>>item;
+     list[numItems] = item;
+     numItems++;
+     }
+    else
+    {
+     cout<<"You'll need a bigger list!"<<endl;
+     }
+    }
+  }while(!(input == 'q'||input == 'Q'));
+   cout<<"==ITEMS TO BUY=="<<endl;
+   for ( int x=0; x< 5; x++)
+   {
+  
+   cout<<x+1<<" "<<list[x]<<endl;
+   }
   return 0;
 }
